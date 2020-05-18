17a89a0366c2f315702644802a34c7a8dfdc5266
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-18 13:29:06 -0700

    Crate relatioship between tables set before action on post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_192156) do
+ActiveRecord::Schema.define(version: 2020_05_18_193551) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
